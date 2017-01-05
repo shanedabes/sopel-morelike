f6bc294e123d8af6d8819f4bea806944497040eb
sharktamer <donohoe.shane@gmail.com>
2017-01-05 17:02:44 +0000

    um

--- morelike.py
+++ morelike.py
@@ -6,7 +6,8 @@ import re
 import random
 
 
-words = ['poo', 'crap', 'shit', 'gay', 'dumb', 'arse', 'fuck', 'piss', 'bum', 'tits']
+words = ['poo', 'crap', 'shit', 'gay', 'dumb', 'arse', 'fuck', 'piss', 'bum',
+         'tits', 'fart', 'dick', 'cock', 'knob', 'ass']
 words = {i: pronouncing.rhyming_part(pronouncing.phones_for_word(i)[0])
          for i in words}
 
@@ -14,32 +15,29 @@ h_en = hyphen.Hyphenator('en_GB')
 
 
 def trans_word(curr_word):
+    if ' ' in curr_word:
+        return curr_word
     wp = pronouncing.phones_for_word(curr_word)[0]
-    print(wp)
-    ws = h_en.syllables(curr_word)
-    print(ws)
-    if not ws:
+    if pronouncing.syllable_count(wp) == 1:
+        ws = [curr_word]
+    else:
+        ws = h_en.syllables(curr_word)
+        if not ws:
+            return curr_word
+    if pronouncing.syllable_count(wp) != len(ws):
         return curr_word
     rw = [(i, words[i]) for i in words if words[i] in wp]
-    print(rw)
     if not rw:
         return curr_word
     rwc = random.choice(rw)
-    print(rwc)
     rwv = re.search(r'(.*[0-9])', rwc[1]).group()
-    print(rwv)
     si = re.findall(r'\w+\d', wp).index(rwv)
     # ws[si] = rwc[0]
     ns = rwc[0]
-    print(ns)
-    print(ws[si])
     if re.search(r'[aeiouy]$', ns):
         ns += re.search(r'[^aeiou]*$', ws[si]).group()
-    print(ns)
     ws[si] = ns
-    print(ws)
     out = ''.join(ws)
-    print(out)
 
     return out
 
