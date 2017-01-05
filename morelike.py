864394435002d560869ec93a0dc4ad5e84fcb92f
sharktamer <donohoe.shane@gmail.com>
2017-01-05 23:53:31 +0000

    add comments and change variable names

--- morelike.py
+++ morelike.py
@@ -16,34 +16,50 @@ h_en = hyphen.Hyphenator('en_GB')
 
 
 def trans_word(curr_word):
+    # return if more than one word is passed
     if ' ' in curr_word:
         return curr_word
     # strip non alphanumeric characters from word
     curr_word = ''.join(i for i in curr_word if i.isalnum())
+    # get the phonemes for the current word
     word_phone_list = pronouncing.phones_for_word(curr_word)
+    # if no phonemes were found return the current word
     if not word_phone_list:
         return curr_word
+    # pick the first pronunciation, it's usually accurate
     word_phones = word_phone_list[0]
+    # if the number of syllables is 1, no need to use hyphen to split
     if pronouncing.syllable_count(word_phones) == 1:
         word_syllables = [curr_word]
     else:
+        # use hyphen to split the word into syllables
         word_syllables = h_en.syllables(curr_word)
+        # if the word couldn't be split, return the original word
         if not word_syllables:
             return curr_word
+    # if hyphen's syllable count doesn't match pronouncing's, return the word
     if pronouncing.syllable_count(word_phones) != len(word_syllables):
         return curr_word
+    # use any of our words that exist in the phoneme for the current word
     rep_words = [(i, words[i]) for i in words if words[i] in word_phones]
+    # if no words match, return the current word
     if not rep_words:
         return curr_word
-    rep_word = random.choice(rep_words)
-    rep_word_vowel = re.search(r'(.*[0-9])', rep_word[1]).group()
-    si = re.findall(r'\w+\d', word_phones).index(rep_word_vowel)
-    new_syllable = rep_word[0]
-    if re.search(r'[aeiouy]$', new_syllable):
-        new_syllable += re.search(r'[^aeiouy]*$', word_syllables[si]).group()
-    word_syllables[si] = new_syllable
-    out = ''.join(word_syllables)
+    # pick one of our matched words to be our new syllable
+    new_syl, new_syl_pronounciation = random.choice(rep_words)
+    # use regex to just find the vowel within our word's phoneme
+    new_syl_vowel = re.search(r'(.*\d)', new_syl_pronounciation).group()
+    # use regex to find the vowel of our phoneme within our current word
+    syllable_index = re.findall(r'\w+\d', word_phones).index(new_syl_vowel)
+    # if our new syllable ends with a vowel, keep the end of the original
+    if re.search(r'[aeiouy]$', new_syl):
+        new_syl += re.search(r'[^aeiouy]*$',
+                                  word_syllables[syllable_index]).group()
+    # using the found index, replace the old syllable with the new one
+    word_syllables[syllable_index] = new_syl
 
+    # recombine and return
+    out = ''.join(word_syllables)
     return out
 
 
