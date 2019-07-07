56fefa3a93e7a1e316a67817f9f9169fb42dec15
sharktamer <donohoe.shane@gmail.com>
2019-07-07 18:27:51 +0100

    fix default formatting

--- tests/test_morelike.py
+++ tests/test_morelike.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
-from __future__ import unicode_literals, absolute_import, division, print_function
+from __future__ import (unicode_literals, absolute_import, division,
+                        print_function)
 
 import unittest
 
 from sopel_modules.morelike import morelike
+
 
 class TestMorelike(unittest.TestCase):
     def setUp(self):
