9560c0e93f245b17580bb64eee85b7c65b32823a
sharktamer <donohoe.shane@gmail.com>
2019-07-07 18:25:54 +0100

    rework structure

---
+++ sopel_modules/morelike/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf8
+"""sopel-morelike
+
+Replace words in message with a list of configurable rhyming words
+"""
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+from .morelike import *
+
+__author__ = 'Shane Donohoe'
+__email__ = 'shane@donohoe.cc'
+__version__ = '0.1.0'
+
